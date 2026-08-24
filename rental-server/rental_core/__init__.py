505b95af0518787d309c7719e997c82f39988c6b
あ <ritsu.1000000@gmail.com>
2026-08-25 08:39:06 +0900

    Export rental contract service

--- rental-server/rental_core/__init__.py
+++ rental-server/rental_core/__init__.py
@@ -1,6 +1,7 @@
 from .config import PLANS, Settings
 from .database import RentalDatabase
 from .manager import RentalManager
+from .rental_service import RentalService
 from .errors import ServiceError
 
-__all__ = ["PLANS", "Settings", "RentalDatabase", "RentalManager", "ServiceError"]
+__all__ = ["PLANS", "Settings", "RentalDatabase", "RentalManager", "RentalService", "ServiceError"]
