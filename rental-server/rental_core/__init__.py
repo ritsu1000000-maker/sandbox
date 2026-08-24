5d15bcdccfa69578a61cd7f4987cef132aec6820
あ <ritsu.1000000@gmail.com>
2026-08-25 08:37:32 +0900

    Export persistent rental database

--- rental-server/rental_core/__init__.py
+++ rental-server/rental_core/__init__.py
@@ -1,5 +1,6 @@
 from .config import PLANS, Settings
+from .database import RentalDatabase
 from .manager import RentalManager
 from .errors import ServiceError
 
-__all__ = ["PLANS", "Settings", "RentalManager", "ServiceError"]
+__all__ = ["PLANS", "Settings", "RentalDatabase", "RentalManager", "ServiceError"]
