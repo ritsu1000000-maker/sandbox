6961c02e40bbed259d12cea87f93068dbf13957b
あ <ritsu.1000000@gmail.com>
2026-08-25 09:39:14 +0900

    Expose Redis database backend

--- rental-server/rental_core/__init__.py
+++ rental-server/rental_core/__init__.py
@@ -1,7 +1,24 @@
 from .config import PLANS, Settings
 from .database import RentalDatabase
+from .redis_database import RedisRentalDatabase
 from .manager import RentalManager
 from .rental_service import RentalService
 from .errors import ServiceError
 
-__all__ = ["PLANS", "Settings", "RentalDatabase", "RentalManager", "RentalService", "ServiceError"]
+
+def build_database(settings):
+    if settings.redis_url:
+        return RedisRentalDatabase(settings.redis_url)
+    return RentalDatabase(settings.database_url)
+
+
+__all__ = [
+    "PLANS",
+    "Settings",
+    "RentalDatabase",
+    "RedisRentalDatabase",
+    "build_database",
+    "RentalManager",
+    "RentalService",
+    "ServiceError",
+]
